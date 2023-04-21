a4861c0df1fa8167deb6665801138822afae6b25
po4tion <po4tion0429@gmail.com>
2023-04-21 11:25:32 +0900

    feat: 기본 세팅

--- tests/lib/rules/eslint-plugin-component.js
+++ tests/lib/rules/eslint-plugin-component.js
@@ -11,45 +11,21 @@
 const rule = require("../../../lib/rules/eslint-plugin-component"),
   RuleTester = require("eslint").RuleTester;
 
+
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester({
-  parser: require.resolve("@babel/eslint-parser"),
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-    requireConfigFile: false,
-    babelOptions: {
-      presets: ["@babel/preset-react"],
-    },
-  },
-});
-
+const ruleTester = new RuleTester();
 ruleTester.run("eslint-plugin-component", rule, {
   valid: [
-    {
-      code: "function ComponentName() { return <div>ComponentName</div>}",
-      filename: "components/ComponentName.jsx",
-    },
-    {
-      code: "const ComponentName = () => { return <div>ComponentName</div>}",
-      filename: "components/ComponentName.jsx",
-    },
+    // give me some code that won't trigger a warning
   ],
+
   invalid: [
     {
-      code: "function componentName() { return <div>ComponentName</div>}",
-      filename: "components/componentName.jsx",
-      errors: [{ messageId: "componentNameError" }],
-      output: "function ComponentName() { return <div>ComponentName</div>}",
-    },
-    {
-      code: "const componentName = () => { return <div>ComponentName</div>}",
-      filename: "components/componentName.jsx",
-      errors: [{ messageId: "componentNameError" }],
-      output: "const ComponentName = () => { return <div>ComponentName</div>}",
+      code: "dont use lowercase",
+      errors: [{ message: "Fill me in.", type: "Me too" }],
     },
   ],
 });
