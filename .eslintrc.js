9bf6c73550e8b8e0747beba9b697f553096cb1a3
po4tion <po4tion0429@gmail.com>
2023-11-29 02:26:13 +0900

    test: jest init

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     "node/no-unpublished-require": [
       "error",
       {
-        allowModules: ["eslint"],
+        allowModules: ["eslint", "jest"],
       },
     ],
   },
